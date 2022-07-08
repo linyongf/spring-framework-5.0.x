499b15a83bcb3ba7167cd83d55b46b4789736885
linyf <linyf@deepcap.cn>
2022-07-08 09:28:54 +0800

    注释ApplicationContext编译错误

--- spring-beans/src/test/java/my_test/aware/AwareTest.java
+++ spring-beans/src/test/java/my_test/aware/AwareTest.java
@@ -1,12 +1,9 @@
 package my_test.aware;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 public class AwareTest {
 	public static void main(String[] args) {
-		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-		Test test = (Test) applicationContext.getBean("test");
-		test.testAware();
+//		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+//		Test test = (Test) applicationContext.getBean("test");
+//		test.testAware();
 	}
 }
